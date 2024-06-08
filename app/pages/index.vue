a4fde031d16e2ddbe0c6ed12a898e4a99f16a6c4
Rajat Chakraborty <102159536+rajatchakrab@users.noreply.github.com>
2024-06-08 19:39:11 +0530

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -11,19 +11,20 @@
             v-else
             @submit.prevent="handleSubmit"
             name="signups"
-            netlify
+            data-netlify="true"
             class="flex items-center border-b border-b-2 border-blue-400 py-2"
           >
+            <input type="hidden" name="form-name" value="signups">
             <input
               ref="emailInput"
               v-model="form.email"
               class="appearance-none mb-36 bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
-              type="text"
+              type="email"
               name="email"
               placeholder="your@email.com"
               aria-label="Email address"
+              required
             />
-
             <button
               class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
@@ -49,17 +50,12 @@ import { Component, Vue } from 'nuxt-property-decorator';
 import settings from '@/content/settings/general.json';
 
 @Component({
-  // Called to know which transition to apply
   transition() {
     return 'slide-left';
   },
 })
 export default class Home extends Vue {
   welcomeText = settings.welcomeText;
-
-  get posts(): Post[] {
-    return this.$store.state.posts;
-  }
 
   isSignedUp = false;
 
@@ -74,8 +70,7 @@ export default class Home extends Vue {
   }
 
   validEmail(email): boolean {
-    // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(email);
   }
 
